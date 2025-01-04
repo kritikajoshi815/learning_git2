94ac3a39158da07abd54dffa5490adcf397c82f4
kritikajoshi815 <kritikajoshi815@gmail.com>
2025-01-04 11:24:25 +0530

    names.txt added

---
+++ JavaScript/app.js
@@ -0,0 +1,19 @@
+// let userName = "Bro";
+// let items = 20;
+// console.log(`Hello ${userName}`);
+// let text =
+//     `Hello ${userName}<br>
+// You have ${items} items in your cart`;
+// console.log(document.getElementById("myLabel").value);
+// document.getElementById("myLabel").innerHTML = text;
+
+//creating objects in js
+{ } //is a blank Object
+var obj = {
+    name: "harsh",
+    age: 24
+}// is an object (key: value pair)
+
+//console.log(obj.name);
+
+//function is an object in js
